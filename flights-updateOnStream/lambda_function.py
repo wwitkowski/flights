a148980415be5418148519c205bf5112ce51661d
wwitkowski <wojciechwitkowski94@gmail.com>
2022-12-17 14:17:22 +0100

    remove comma

--- flights-updateOnStream/lambda_function.py
+++ flights-updateOnStream/lambda_function.py
@@ -13,7 +13,7 @@ from .table import FlightsTable
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 dyn_resource = boto3.resource(
-    'dynamodb',
+    'dynamodb'
     # aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
     # aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
     # region_name='us-east-1'
