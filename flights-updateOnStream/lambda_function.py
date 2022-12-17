df1333b83812613e1e848d3cd751d1e3458bb145
wwitkowski <wojciechwitkowski94@gmail.com>
2022-12-17 14:15:04 +0100

    add feature sending mail

--- flights-updateOnStream/lambda_function.py
+++ flights-updateOnStream/lambda_function.py
@@ -1,51 +1,131 @@
 import os
+import smtplib
+import ssl
 import boto3
 import time
 import logging
-import statistics
+import numpy as np
+from datetime import datetime 
+from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEMultipart 
 from .table import FlightsTable
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 dyn_resource = boto3.resource(
-    'dynamodb'
+    'dynamodb',
     # aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    # aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+    # aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    # region_name='us-east-1'
 )
 table = dyn_resource.Table('flights')
+origin_map = {
+    'KTW': 'Katowice',
+    'KRK': 'Kraków',
+    'WAW': 'Warszawa'
+}
+
+
+def flight_to_text(flight):
+    fire_emojis = '&#128293;'*flight['level']
+    origin = flight['flight_details']['origin_place_id']
+    destination = flight['flight_details']['destination_place_id']
+    destination_name = f"{flight['flight_details']['destination_city']}, {flight['flight_details']['destination_country']}"
+    departure_date = datetime.strptime(flight['current_flight']['departure_date'], '%Y%m%d').date().isoformat()
+    return_date = datetime.strptime(flight['current_flight']['return_date'], '%Y%m%d').date().isoformat()
+    days = flight['current_flight']['days']
+    current_price = flight['current_flight']['price']
+    mean_price = flight['mean_price']
+    return f"<p>{fire_emojis} {origin} - {destination} ({destination_name}): "\
+        f"Od {departure_date} do {return_date} ({days} dni) za <b>{current_price} zł</b> (Średnia: {mean_price:.2f} zł)</p>"
+
+
+def send_email(flights):
+    sender_email = os.environ.get('GMAIL_FROM')
+    receiver_email = os.environ.get('GMAIL_TO').split(',')
+    title_origin = origin_map[flights[0]['flight_details']['origin_place_id']]
+    date = datetime.today().date().isoformat()
+    password = os.environ.get('GMAIL_PASSWORD')
+    
+    message = MIMEMultipart("alternative") 
+    message["Subject"] = f"Tanie loty {title_origin} - {date}" 
+    message["From"] = sender_email 
+    message["To"] = ', '.join(receiver_email)
+    html = f"""
+    <html> 
+        <body> 
+            {'<br>'.join([flight_to_text(flight) for flight in flights])}
+        </body> 
+    </html> 
+    """ 
+    message_body = MIMEText(html, "html") 
+    message.attach(message_body) 
+    context = ssl.create_default_context() 
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: 
+        server.login(sender_email, password) 
+        server.sendmail( 
+            sender_email, receiver_email, message.as_string()
+        )
+
 
 def lambda_handler(event, context):
-
     i = 0
     table = FlightsTable(dyn_resource)
+    cheap_flights = []
     if table.exists():
         records = event['Records']
         for record in records:
             if record['eventName'] != 'INSERT':
-                msg = 'Not an insert event.'
-                logger.info(msg)
                 continue
             if record['dynamodb']['NewImage']['SortKey']['S'] in ['total_agg', 'details']:
-                msg = 'Not a price item.'
-                logger.info(msg)
                 continue
+            logger.info(record)
             flight_id = record['dynamodb']['Keys']['FlightID']['S']
             new_price = record['dynamodb']['NewImage']['price']['N']
+            current_flight = {
+                'price': new_price,
+                'days': record['dynamodb']['NewImage']['days']['N'],
+                'departure_date': record['dynamodb']['NewImage']['departure_date']['S'],
+                'return_date': record['dynamodb']['NewImage']['return_date']['S']
+            }
             response = table.query_items('FlightID', flight_id)
-            prices = [item['price'] for item in response if 'cid' in item['SortKey']]
+            flight_info = [item for item in response if 'details' in item['SortKey']][0]
+            prices = [float(item['price']) for item in response if 'cid' in item['SortKey']]
             if len(prices) < 5:
-                logger.info('Not enough prices for %s', flight_id)
                 continue
-            mean_price = statistics.mean(prices)
-            thrshold = mean_price - 2*statistics.stdev(prices)
-            if float(new_price) < float(thrshold):
-                logger.info(
-                    '!!!! Found cheap flight! Flight: %s, Mean price: %s, Current price: %s', 
-                    flight_id, mean_price, new_price
-                    )
+            mean_price = np.mean(prices)
+            q3, q1 = np.percentile(prices, [75, 25])
+            iqr = q3 - q1
+            thrshold_lvl1 = q1 - 0.5 * iqr
+            thrshold_lvl2 = q1 - 1 * iqr
+            thrshold_lvl3 = q1 - 1.5 * iqr
+            cheap_flight = {
+                    'flight_details': flight_info, 
+                    'mean_price': mean_price, 
+                    'current_flight': current_flight, 
+                }
+            if new_price < thrshold_lvl3:
+                cheap_flights.append({
+                    'level': 3,
+                    **cheap_flight
+                })
+            elif new_price < thrshold_lvl2:
+                cheap_flights.append({ 
+                    'level': 2,
+                    **cheap_flight
+                })
+            elif new_price < thrshold_lvl1:
+                cheap_flights.append({
+                    'level': 1,
+                    **cheap_flight
+                })
             i += 1
             time.sleep(0.15)
     logger.info('Checked %d items', i)
+    if cheap_flights:
+        logger.info('Sending mail')
+        send_email(cheap_flights)
+    logger.info('Done')
     return {
         'statusCode': 200,
     } 
